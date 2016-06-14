ff28e8a83efc5299908a8938a1009d28259977b2
unknown <dheerajv@dheerajv.cybage.com>
2016-06-14 20:08:45 +0530

    Initial commit
    cfa8382 Initial commit
    
    commit changes
    
    commit changes
    
    Remove comment
    
    commit change

--- src/Dateandtime/Date1.java
+++ src/Dateandtime/Date1.java
@@ -1,10 +1,21 @@
 package Dateandtime;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Date1{
 public static void main(String args[]){
 	Date date = new Date();
-	System.out.println(date);
+	
+	
+	 SimpleDateFormat ft = 
+		      new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+	//printing date format
+         System.out.println(date);
+        //printing simple date format 
+         System.out.println("Current Date: " + ft.format(date));
+      //printing date and simple date format    
+System.out.println(date+""+"Current Date: " + ft.format(date));
+
 }
 }
